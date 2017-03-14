f6f78729b49f79ff329684a886508fb41d9d0a22
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-03-14 19:14:52 +0100

    chore: missing files at root

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  plugins: [],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // This rule is required because atom vue-format package remove the space
+    'space-before-function-paren': 0
+  },
+  globals:{
+    localStorage: true
+  }
+}

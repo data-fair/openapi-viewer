97239d95ec45b4b8b8d197c35cbe0b0ddeb50772
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2017-08-16 11:44:32 +0200

    feat: updated openapi-viewer version, url can be prefilled with a query parameter

--- public/src/App.vue
+++ public/src/App.vue
@@ -5,20 +5,55 @@
       <md-layout md-flex="50" md-flex-offset="25">
         <md-input-container>
           <label>OpenAPI JSON location</label>
-          <md-input v-model="url"></md-input>
+          <md-input v-model="url" @keyup.enter.native="refresh"></md-input>
         </md-input-container>
       </md-layout>
       <md-layout>
-        <md-button @click.native="refresh"><md-icon>autorenew</md-icon></md-button>
+        <md-button @click.native="refresh">
+          <md-icon>autorenew</md-icon>
+        </md-button>
       </md-layout>
     </md-layout>
   </md-toolbar>
-  <open-api v-if="api" :api="api"></open-api>
+  <open-api v-if="api && !error" :api="api"></open-api>
+  <md-layout md-row md-align="center" v-if="error" style="margin-top:64px">
+    <md-layout md-flex="50" md-flex-offset="25">
+      <md-whiteframe md-elevation="2" style="padding:24px">Cannot retrieve data for this URL or JSON is not well formed</md-whiteframe>
+    </md-layout>
+  </md-layout>
 </div>
 </template>
 
 <script>
 import OpenApi from 'vue-openapi'
+
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+  if (!url) {
+    url = window.location.href
+  }
+  name = name.replace(/[\[\]]/g, '\\$&')
+  let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+  let results = regex.exec(url)
+  if (!results) {
+    return null
+  }
+  if (!results[2]) {
+    return ''
+  }
+  return decodeURIComponent(results[2].replace(/\+/g, ' '))
+}
+
+// https://stackoverflow.com/questions/5999118/add-or-update-query-string-parameter
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, '$1' + key + "=" + value + '$2');
+  } else {
+    return uri + separator + key + "=" + value;
+  }
+}
 
 export default {
   name: 'app',
@@ -27,15 +62,31 @@ export default {
   },
   data: () => ({
     api: null,
-    url: 'https://koumoul.com/s/geocoder/api/v1/api-docs.json'
+    error: null,
+    url: getParameterByName('url') || 'https://koumoul.com/s/geocoder/api/v1/api-docs.json'
   }),
   mounted() {
     this.refresh()
   },
   methods: {
     refresh() {
-      this.$http.get('./proxy?url=' + this.url).then(response => response.json()).then(api => this.api = api)
+      let newUrl = updateQueryStringParameter(window.location.href, 'url', this.url)
+      if (newUrl !== window.location.href) {
+        window.location.href = newUrl
+      }
+      this.$http.get('./proxy?url=' + this.url).then(response => response.json()).then(api => {
+        this.api = api
+        this.error = null
+      }, error => {
+        this.error = error
+      })
     }
   }
 }
 </script>
+
+<style>
+.md-input-container input {
+  margin-top: 14px;
+}
+</style>

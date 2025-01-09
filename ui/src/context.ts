a5ba8fd4b7200539773317dc0d98375bbd595513
BatLeDev <baptiste.guerny@gmail.com>
2025-01-09 15:52:16 +0100

    feat: fetch api-docs.json

--- ui/src/context.ts
+++ ui/src/context.ts
@@ -3,4 +3,4 @@ import type { UiConfig } from '../../api/src/config'
 
 export const $uiConfig = (window as any).__UI_CONFIG as UiConfig
 export const $sitePath = (window as any).__SITE_PATH as string
-export const $fetch = ofetch.create()
+export const $fetch = ofetch.create({ baseURL: $sitePath })
--- ui/src/pages/index.vue
+++ ui/src/pages/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <top-bar v-if="$route.query.header !== 'false'"/>
+  <top-bar v-if="$route.query['hide-toolbar'] !== 'false'"/>
   <h1>Hello world</h1>
+  <v-alert
+    v-if="!url.startsWith('http://') && !url.startsWith('https://')"
+    type="warning"
+    variant="outlined"
+    :text="'URL must start with http:// or https://'"
+  />
+  <v-alert
+    v-if="urlFetch.error.value"
+    type="error"
+    variant="outlined"
+    :text="urlFetch.error.value"
+  />
+  <pre>{{ urlFetch.data.value }}</pre>
 </template>
 
 <script setup lang="ts">
+const url = useStringSearchParam('url')
+const urlFetch = useFetch(url, { notifError: false })
+
+// TODO validate urlFetch.data.value
 </script>
 
 <style scoped>

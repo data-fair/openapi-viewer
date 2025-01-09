9d629cf887f965e5f447fad8755a8d6d3f444d4c
BatLeDev <baptiste.guerny@gmail.com>
2025-01-09 11:43:30 +0100

    feat: setup the server

---
+++ api/src/app.ts
@@ -0,0 +1,20 @@
+import { resolve } from 'node:path'
+import express from 'express'
+import { errorHandler, createSiteMiddleware, createSpaMiddleware } from '@data-fair/lib-express'
+import config, { uiConfig } from '#config'
+
+export const app = express()
+
+// no fancy embedded arrays, just string and arrays of strings in req.query
+app.set('query parser', 'simple')
+app.use(createSiteMiddleware('customers'))
+
+app.set('json spaces', 2)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+if (config.serveUi) {
+  app.use(await createSpaMiddleware(resolve(import.meta.dirname, '../../ui/dist'), uiConfig))
+}
+
+app.use(errorHandler)
---
+++ api/src/config.ts
@@ -0,0 +1,15 @@
+import type { ApiConfig } from '../config/type/index.ts'
+import { assertValid } from '../config/type/index.ts'
+import config from 'config'
+
+// we reload the config instead of using the singleton from the config module for testing purposes
+// @ts-ignore
+const apiConfig = process.env.NODE_ENV === 'test' ? config.util.loadFileConfigs(process.env.NODE_CONFIG_DIR, { skipConfigSources: true }) : config
+assertValid(apiConfig, { lang: 'en', name: 'config', internal: true })
+
+config.util.makeImmutable(apiConfig)
+
+export default apiConfig as ApiConfig
+
+export const uiConfig = {}
+export type UiConfig = typeof uiConfig

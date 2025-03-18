088c3a412378f7f12c9af43b6112a66f740b7f94
Alban Mouton <alban.mouton@gmail.com>
2025-03-18 16:18:47 +0100

    chore: more tolerant site middleware from lib-express

--- api/src/app.ts
+++ api/src/app.ts
@@ -7,7 +7,7 @@ export const app = express()
 
 // no fancy embedded arrays, just string and arrays of strings in req.query
 app.set('query parser', 'simple')
-app.use(createSiteMiddleware('openapi-viewer', true))
+app.use(createSiteMiddleware('openapi-viewer', { prefixOptional: true, neverInternal: true }))
 
 app.set('json spaces', 2)
 app.use(express.json())
